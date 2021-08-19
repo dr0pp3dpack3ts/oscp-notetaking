f7da2e677d7379b11d2952e9a0e310e9b402fc27
droppedpackets <56236210+dr0pp3dpack3ts@users.noreply.github.com>
2021-08-19 14:01:13 -0700

    Update generate_report.sh

--- generate_report.sh
+++ generate_report.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-	echo "usage: $0 <input.md> <output.pdf>"
+### Basic if statement (if this is true then do this and if not true dont do that)
+if [ "$#" -ne 2 ]; then ### if there is no input then print in line 2...
+	echo "usage: $0 <input.md> <output.pdf>" ### prints out input for md filename and pdf filename.
 	exit
-fi
+fi 
 
 # if this machine does not have the template in place, put it there!
 if [ ! -e /usr/share/pandoc/data/templates/eisvogel.latex ]
